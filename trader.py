fa34892a0b35cc329ad5163debe0aab698929740
Thelyricsman01 <thelyricsman01@gmail.com>
2026-03-26 21:44:08 +0100

    .

--- trader.py
+++ trader.py
@@ -11,6 +11,7 @@ from datetime import datetime
 
 import os
 print("Key exists:", bool(os.getenv("ANTHROPIC_API_KEY")))
+print(repr(os.getenv("ANTHROPIC_API_KEY")))
 
 api_key = "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"
 client  = anthropic.Anthropic(api_key=api_key)
